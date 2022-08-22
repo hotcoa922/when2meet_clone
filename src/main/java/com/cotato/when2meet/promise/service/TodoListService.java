81d8cc935d77304b416c980489aec4abf07fbced
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2022-08-22 11:39:45 +0900

    임시

--- src/main/java/com/cotato/when2meet/promise/service/TodoListService.java
+++ src/main/java/com/cotato/when2meet/promise/service/TodoListService.java
@@ -1,10 +1,10 @@
-package com.cotato.when2meet.promise.todo;
+package com.cotato.when2meet.promise.service;
 
-import com.cotato.when2meet.promise.member.Member;
+import com.cotato.when2meet.promise.todo.Todo;
 
 import java.util.List;
 
-public interface TodoService {
+public interface TodoListService {
     void join_todo(Todo todo); // 일정 생성
     Todo findTodo(Long todoId); // 일정 조회
     void join_member(Long memberId, Long todoId); // 회원 추가
